1db78037b143d97d4574ab32e686d87eca5ecd37
surya <109945988+Surya9510@users.noreply.github.com>
2023-04-14 11:43:54 +0530

    new task component added

--- src/App.js
+++ src/App.js
@@ -1,42 +1,50 @@
-import "./App.css";
-import Cart from "./Components/Filter";
-import Delete from "./Components/Delete";
-import Header from "./Components/Header/Header";
-import Post from "./Components/Post";
-import Users from "./Components/Users";
-import Todo from "./Components/Todo";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Comments from "./Components/Header/Comments";
-import Quotes from "./Components/Quotes";
-import Material from "./Components/MaterialUi/Material";
-import Spinner from "./Components/Spinner/Spinner";
-import { useEffect, useState } from "react";
+// import "./App.css";
+// import Cart from "./Components/Filter";
+// import Delete from "./Components/Delete";
+// import Header from "./Components/Header/Header";
+// import Post from "./Components/Post";
+// import Users from "./Components/Users";
+// import Todo from "./Components/Todo";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import Comments from "./Components/Header/Comments";
+// import Quotes from "./Components/Quotes";
+// import Material from "./Components/MaterialUi/Material";
+// import Spinner from "./Components/Spinner/Spinner";
+// import { useEffect, useState } from "react";
+import Task from "./Task/Task";
 
-function App() {
-const[isLoading,setIsLOading] = useState(false);
 
-useEffect( () => {
-  setIsLOading(true);
-  setIsLOading(false)
-},[])
+
+const App = () => {
+// const[isLoading,setIsLOading] = useState(false);
+
+// useEffect( () => {
+//   setIsLOading(true);
+//   setIsLOading(false)
+// },[])
 
   return (
-    <div>
-      <BrowserRouter>
-        <Header />
-        <Material />
-        <Routes>
-          <Route path="/Cart" element={<Cart />} />
-          <Route path="/Delete" element={<Delete />} />
-          <Route path="/Post" element={<Post />} />
-          <Route path="/Users" element={<Users />} />
-          <Route path="/Comments" element={<Comments />} />
-          <Route path="/Todo" element={<Todo />} />
-          <Route path="/Quotes" element={<Quotes />} />
-        </Routes>
-      </BrowserRouter>
-      {isLoading ? <p>Application Loaded Successfully</p> : <Spinner />}
+//     <div>
+//       <BrowserRouter>
+//         <Header />
+//         <Material />
+//         <Routes>
+//           <Route path="/Cart" element={<Cart />} />
+//           <Route path="/Delete" element={<Delete />} />
+//           <Route path="/Post" element={<Post />} />
+//           <Route path="/Users" element={<Users />} />
+//           <Route path="/Comments" element={<Comments />} />
+//           <Route path="/Todo" element={<Todo />} />
+//           <Route path="/Quotes" element={<Quotes />} />
+//         </Routes>
+//       </BrowserRouter>
+//       {isLoading ? <p>Application Loaded Successfully</p> : <Spinner />}
       
+    // </div>
+
+
+    <div>
+     <Task />
     </div>
   );
 }
